fb981fa158b74bd70945deef986faff93692cb0f
Juan A. Vilchez Huarcaya <juanvilchez2002@gmail.com>
2021-09-21 00:45:33 -0500

    adicionando efectos de scrollbar

--- Pagina02/js/main.js
+++ Pagina02/js/main.js
@@ -1,3 +1,12 @@
 document.querySelector('.menu-btn').addEventListener('click',() => {
     document.querySelector('.nav-menu').classList.toggle('show')
 });
+
+/*
+    adicionando el efecto para que pueda aparecer de poco a poco  las secciones en la pagina
+*/
+
+ScrollReveal().reveal('.showcase', {delay: 500});
+ScrollReveal().reveal('.cards-banner-one', {delay: 500});
+ScrollReveal().reveal('.new-cards', {delay: 500});
+ScrollReveal().reveal('.cards-banner-two', {delay: 500});
